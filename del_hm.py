5c8af577343e4368ac6dd6d995b974af537631f1
s183983 <s183983@student.dtu.dk>
2022-05-03 10:56:59 +0200

    Update del_hm.py

--- del_hm.py
+++ del_hm.py
@@ -11,7 +11,8 @@ import vtk
 import numpy as np
 
 path = r"C:\Users\lowes\OneDrive\Skrivebord\DTU\8_semester\Advaced_Geometric_DL\BU_3DFE_full"
-# path = "/scratch/s183983/data_cropped/" 
+path = "/scratch/s183983/data_cropped/" 
+"""
 vtk_files = glob.glob(os.path.join(path,"**/*.vtk"))
 
 lp = []
@@ -31,3 +32,21 @@ for file in tqdm.tqdm(vtk_files):
 print("minimum number of points in data is", np.array(lp).min())
 
 print("maximum number of points in data is", np.array(lp).max())
+"""
+lab_tmp_dir = os.path.join(path, "labels_tmp")
+
+files = glob.glob(os.path.join(lab_tmp_dir,"*.npz"))
+for file in tqdm.tqdm(files):
+    
+    loaded = np.load(file)
+    lab_84 = loaded["labels"]
+    
+    full_file = os.path.join(path, "labels", os.path.basename(file))
+    full_loaded = np.load(full_file)
+    lab = loaded["labels"]
+    tex = loaded["texture"]
+    lms = loaded["landmarks"]
+    
+    labels = np.vstack((lab, lab_84))
+    
+    np.savez(full_file, labels = labels, texture = tex, landmarks=lms)
